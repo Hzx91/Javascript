070980b4834dda5624ff291acdb642e26f836f6e
黄梓星 <3274801970@qq.com>
2025-12-30 15:00:20 +0800

    Update ToDoList.html

---
+++ first-day/first.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2025-03-05 21:38:10
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2025-07-20 13:13:48
+ * @FilePath: \Javascript\first-day\first.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log("Hello,Everyone!")
+
+
+/**
+ * @description: 
+ * @param {*} a
+ * @param {*} b
+ * @return {*}
+ */
+function sum(a, b) {
+    return a + b;
+}
