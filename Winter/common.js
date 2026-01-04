68c17b99a4a3b19e810d6711b63e6314aaad0d4d
黄梓星 <3274801970@qq.com>
2026-01-04 12:10:52 +0800

    调整了注释格式

--- Winter/common.js
+++ Winter/common.js
@@ -2,6 +2,7 @@
 function getStyle(element, property) {
     // 用条件三元运算符判断代替if语句
     return window.getComputedStyle ? //判断是否存在/为真
-        window.getComputedStyle(element, null)[property] //为真：就用现代浏览器getComputedStyle
-        : element.currentStyle[property];//为假：使用旧版IEcurrentStyle属性
+        window.getComputedStyle(element, null)[property] : element.currentStyle[property];
+    //为真：就用现代浏览器getComputedStyle方法
+    //为假：使用旧版IEcurrentStyle属性
 }
